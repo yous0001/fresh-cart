d705a1ca59c8008a197a009c12127f4deb653d38
Yousef emad mikhail <mad33833@gmail.com>
2024-09-28 22:17:58 +0300

    add not found component

--- src/Components/Notfound/Notfound.jsx
+++ src/Components/Notfound/Notfound.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import error404 from "../../assets/error.svg"
 
 export default function Notfound() {
   return (
-    <div>Notfound</div>
+    <div className='text-center py-5'>
+      <img src={error404} alt='page not found' />
+    </div>
   )
 }
