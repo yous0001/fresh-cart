5d5003fd5c5276e7689b544c1ee44d4fdd92b5a7
Yousef emad mikhail <mad33833@gmail.com>
2024-09-29 17:07:19 +0300

    add: create register component design

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -2,6 +2,33 @@ import React from 'react'
 
 export default function Register() {
   return (
-    <div>Register</div>
+    <div className='w-75 mx-auto'>
+      <h2 className='my-3 fw-bold'>Register Now:</h2>
+      <form action="">
+        <div className='mb-3'>
+          <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="name" className='form-control'/>
+        </div>
+        <div className='mb-3'>
+          <label htmlFor="email">email</label>
+          <input type="email" name="email" id="email" className='form-control'/>
+        </div>
+        <div className='mb-3'>
+          <label htmlFor="phone">phone</label>
+          <input type="tel" name="phone" id="phone" className='form-control'/>
+        </div>
+        <div className='mb-3'>
+          <label htmlFor="password">password</label>
+          <input type="password" name="password" id="password" className='form-control'/>
+        </div>
+        <div className='mb-3'>
+          <label htmlFor="repassword">repassword</label>
+          <input type="password" name="repassword" id="repassword" className='form-control'/>
+        </div>
+
+        
+        <button className='btn text-light bg-main float-end' type="submit">submit</button>
+      </form>
+    </div>
   )
 }
